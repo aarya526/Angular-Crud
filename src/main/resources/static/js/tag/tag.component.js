a0b16301c97ee2cd48eef89d089b212ad559e956
mac <mac@macs-MacBook-Pro.local>
2020-06-25 20:55:19 +0530

    tag

--- src/main/resources/static/js/tag/tag.component.js
+++ src/main/resources/static/js/tag/tag.component.js
@@ -6,31 +6,31 @@ tag.component('addTag', {
 
 		$scope.tag = {};
 		$scope.tags = [
-			/*			{
-			
-						"name": "Abhishek Arya",
-						"phone": "971711363",
-						"subject": "Economics"
-			
-					}, {
-			
-						"name": "Abhishek Arya",
-						"phone": "971711363",
-						"subject": "Economics"
-			
-					}, {
-			
-						"name": "Abhishek Arya",
-						"phone": "971711363",
-						"subject": "Economics"
-			
-					}, {
-			
-						"name": "Abhishek Arya",
-						"phone": "971711363",
-						"subject": "Economics"
-			
-					}*/
+			/*{
+
+				"name": "Abhishek Arya",
+				"phone": "971711363",
+				"subject": "A"
+
+			}, {
+
+				"name": "Abhishek Arya",
+				"phone": "971711363",
+				"subject": "B"
+
+			}, {
+
+				"name": "Abhishek Arya",
+				"phone": "971711363",
+				"subject": "C"
+
+			}, {
+
+				"name": "Abhishek Arya",
+				"phone": "971711363",
+				"subject": "D"
+
+			}*/
 		];
 
 		$scope.submitTag = function() {
@@ -45,6 +45,17 @@ tag.component('addTag', {
 
 			$scope.tag = {};
 
+		}
+
+		$scope.delete = function(tag) {
+
+			var removeIndex = $scope.tags.indexOf(tag);
+			if (removeIndex != -1) {
+
+				console.log($scope.tags[removeIndex]);
+				$scope.tags.splice(removeIndex, 1);
+
+			}
 		}
 
 	}
